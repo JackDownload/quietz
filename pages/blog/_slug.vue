028917fe9a9e3e7f84ae0867d3590e1ddf3dfa74
JackDownload <kismocsy@gmail.com>
2021-06-20 13:39:49 +0200

    alug2

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -10,8 +10,7 @@ export default {
   async asyncData({ $content, params, error }) {
     let post;
     try {
-      post = await $content("blog", params.slug).fetch();
-      // OR const article = await $content(`articles/${params.slug}`).fetch()
+      const blog = await $content(`blog/${params.slug}`).fetch();
     } catch (e) {
       error({ message: "Blog Post not found" });
     }
